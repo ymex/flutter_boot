e2190270a1cf4da735c9987160a99d04062768f5
JD <JD>
2023-09-27 10:24:16 +0800

    effect

--- lib/src/widget/effect.dart
+++ lib/src/widget/effect.dart
@@ -40,3 +40,56 @@ class _FadeEffectState extends State<FadeEffect> {
         ));
   }
 }
+
+
+
+class AnimationFadeEffect extends StatefulWidget {
+  final Widget child;
+  final GestureTapCallback? onTap;
+  final double opacity;
+
+  final int duration; //milliseconds
+
+  const AnimationFadeEffect(
+      {super.key,
+        required this.child,
+        this.onTap,
+        this.opacity = 0.6,
+        this.duration = 150});
+
+  @override
+  State<AnimationFadeEffect> createState() => _AnimationFadeEffectState();
+}
+
+class _AnimationFadeEffectState extends State<AnimationFadeEffect>
+    with SingleTickerProviderStateMixin {
+  double opacity = 1;
+  late final AnimationController _controller = AnimationController(
+      vsync: this, duration: Duration(milliseconds: widget.duration));
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (widget.onTap != null) {
+          widget.onTap!();
+        }
+      },
+      onTapUp: (event) {
+        _controller.reset();
+      },
+      onTapDown: (event) {
+        _controller.forward();
+      },
+      child: FadeTransition(
+          opacity: Tween(begin: 1.0, end: widget.opacity).animate(_controller),
+          child: widget.child),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
