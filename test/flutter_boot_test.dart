ad0592928a171791b25eee8f9f84b1aed09fc46f
ymex <ymex@foxmail.com>
2023-10-01 00:35:59 +0800

    test

--- test/flutter_boot_test.dart
+++ test/flutter_boot_test.dart
@@ -1,27 +1,6 @@
-import 'package:flutter_boot/lifecycle.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-mixin Am on Object{
-  @override
-  String toString() {
-    print('-----${super.toString()}');
-    return "am ----";
-  }
-}
-
-mixin Bx on Object{
-  @override
-  String toString() {
-    print('-----${super.toString()}');
-    return "bm ----";
-  }
-}
-
-class Userm with Am,Bx{}
-
 void main() {
-  var u = Userm();
-  print(' '+u.toString());
-  // test('adds one to input values', () {
-  // });
+  test('adds one to input values', () {
+  });
 }
